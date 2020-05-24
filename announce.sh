79ce5236d5d2febf1e8cb82b18d77c4f9c5f8be6
HimbeerserverDE <52707839+HimbeerserverDE@users.noreply.github.com>
2020-05-24 17:18:08 +0200

    Updated authentication mechanism

--- announce.sh
+++ announce.sh
@@ -3,7 +3,7 @@
 # serverID=$(ifconfig -a | awk '/^[a-z]/ { iface=$1; mac=$NF; next } /inet addr:/ { print mac }')
 serverID=$(ifconfig | grep ether | sed -n '1 p' | awk '{print $2}')
 serverID=${serverID//:/}
-serverName="HimbeerserverDE"
+serverName="Speedtest server"
 hostName=$(hostname)
 
 wget "http://server.himbeerserver.de/speedtest/announce.php?m=a&i=$serverID&n=$serverName&a=$hostName" -O /tmp/announceresponse.html
